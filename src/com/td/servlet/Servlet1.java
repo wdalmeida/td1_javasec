c9b5572ef2654543d692f559d6e15e272e0f0464
wdalmeida <wdalmeida.dev@gmail.com>
2020-12-21 12:31:51 +0100

    TP1 error 404

---
+++ src/com/td/servlet/Servlet1.java
@@ -0,0 +1,53 @@
+package com.td.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try{
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			String n=request.getParameter("userName");
+			out.print("Welcome "+n);
+			HttpSession session=request.getSession();
+			session.setAttribute("uname",n);
+			out.print(" <a href='profile.jsp'>visit</a>");
+			out.close();
+			}catch(Exception e){
+				System.out.println(e);}
+			}
+
+}
